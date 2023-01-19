88e4305e1949f2951b360f8c870cc1449d43ab3a
HUONG <nguyenhuuhuong1602@gmail.com>
2023-01-19 08:49:33 +0700

    update

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -48,7 +48,7 @@ pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_nfts;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -276,7 +276,7 @@ impl pallet_sudo::Config for Runtime {
 }
 
 /// Configure the pallet-template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl pallet_nfts::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
@@ -297,7 +297,7 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
-		TemplateModule: pallet_template,
+		NftsModule: pallet_nfts,
 	}
 );
 
@@ -344,7 +344,7 @@ mod benches {
 		[frame_system, SystemBench::<Runtime>]
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
-		[pallet_template, TemplateModule]
+		[pallet_nfts, NftsModule]
 	);
 }
 
